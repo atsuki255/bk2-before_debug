c82898e81e1e0ad3798ba4800d40c9235e65b12c
atsuki225 <atsuki443@gmail.com>
2020-09-08 17:54:20 +0900

    [finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,69 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+   before_action :ensure_current_user, {only: [:edit,:update,:destroy]}
+
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+
+  def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @user = current_user
+    @book = Book.new(book_params)
+    @book.user_id = (current_user.id)
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      flash[:notice] = "errors prohibited this obj from being saved:"
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      flash[:notice]= ' errors prohibited this obj from being saved:'
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+    params.require(:user).permit(:name,:image,:introduction)
+  end
+
+  def  ensure_current_user
+      @book = Book.find(params[:id])
+     if @book.user_id != current_user.id
+        redirect_to books_path
+     end
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_current_user, {only: [:edit,:update,:destroy]}
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to "/users/#{current_user.id}"
+      flash[:notice] = "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def book_params
+        params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_current_user
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+end 
